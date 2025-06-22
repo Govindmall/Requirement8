3d8691d27e7c93e9dfbeae3a3bb2a6578d307706
Govindmall <govind.mall0704@gmail.com>
2025-06-22 11:48:07 +0530

    Display the number of orders for each item.

--- src/main/scala/Requirement8.scala
+++ src/main/scala/Requirement8.scala
@@ -6,8 +6,8 @@ object Requirement8{
     val spark = SparkSession.builder().appName("DistinctCountries").master("local[*]").getOrCreate()
     //read the csv file into DataFrame
     val salesDF = spark.read.option("header", "true").csv("C:/Users/gomall/Downloads/sparsales.csv")
-    //group by item type and count the number of orders
     val salesDF1=salesDF.coalesce(1)
+    //group by item type and count the number of orders
     val itemOrdersDF=salesDF1.groupBy("Item Type").agg(count("Order ID").as("Number_of_orders")).persist()
     itemOrdersDF.show()
     itemOrdersDF.write.partitionBy("Item Type").parquet("C:/Users/gomall/Desktop/requirement8.parquet")
